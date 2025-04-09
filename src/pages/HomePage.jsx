4d0e1d2c61a8748bce25e1876d54c17ad30beb43
Dulani-Viyathma <dulanii2002@gmail.com>
2025-04-09 21:11:27 +0530

    Editing homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to Flavor Exchange</h1>
+        <p>This is where all recipes will be shown</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
